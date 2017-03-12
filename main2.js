a6b1f60eabb3881308d1adfc58bcd05663c62024
Thien Tran <thientran@Thiens-MacBook-Pro.local>
2017-03-12 14:15:18 -0700

    updates

--- main2.js
+++ main2.js
@@ -3,32 +3,89 @@ var yourpizza = "";
 function newList(order) {
   yourpizza = yourpizza + order;
   document.getElementById("userOrder").innerHTML = yourpizza;
+  
 }
 newList("")
 
 var pizzaTotal = 0;
+var tax = 0;
+var fprice = 0;
 
 function updateTotal(cost) {
   pizzaTotal = pizzaTotal + cost;
+  tax = tax + (pizzaTotal*0.12);
+  fprice = tax + pizzaTotal;
   document.getElementById("pizzaTotal").innerHTML = pizzaTotal;
+  document.getElementById("tax").innerHTML = tax;
+  document.getElementById("test").innerHTML = fprice;
+  //alert("Total Price Is:" + fprice);
+ // return pizzaTotal;
+
 }
-updateTotal(0)
+ 
+
+updateTotal(0);
+//totalTax(0);
 document.getElementById("bellpepper").onclick = function() {
   updateTotal(2);
   newList("Bell Pepper");
+  //totalTax(1);
+  //finalprice(tax,pizzaTotal);
+};
+document.getElementById("mushrooms").onclick = function() {
+  updateTotal(1);
+  newList("Mushrooms");
+  //totalTax(1);
+  //finalprice(tax,pizzaTotal);
 };
 document.getElementById("pepperoni").onclick = function() {
-  updateTotal(3);
+  updateTotal(3)
   newList("Pepperoni");
+  //totalTax(2);
+ // finalprice(tax,pizzaTotal);
 };
-document.getElementById("mushroom").onclick = function() {
-  updateTotal(1)
-  newList("Mushrooms");
-};
+
 document.getElementById("clear").onclick = function(){
     pizzaTotal = 0;
     yourpizza = "";
   newList("");
   updateTotal(0);
+  //totalTax(0);
+  //finalprice(tax,pizzaTotal);
   console.log("testclear")
 };
+	function placeOrder() {
+  
+  var myString = document.getElementById("order").value;
+	var Order = myString.split(",");
+
+  for (var i=0; i<Order.length; i++) {
+		Order[i] = Order[i].trim();
+    if( Order[i] == "mushrooms")
+    {
+      updateTotal(1);
+    newList("Mushrooms, ");
+  }
+  else if ( Order[i] == "bellpepper")
+    {
+      updateTotal(2);
+    newList("Bell Pepper, ");
+  }
+  else if ( Order[i] == "pepperoni")
+    {
+      updateTotal(3);
+    newList("Pepperoni, ");
+  }
+
+    else
+      document.getElementById("userOrder").innerHTML = "topping not test available";
+    
+}
+};
+
+
+
+
+
+
+
